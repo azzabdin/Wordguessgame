46bc305fb18547dca2bd62eed7588ba8854565d4
azza <azzabdin@gmail.com>
2019-02-26 22:27:01 -0500

    m

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,34 +2,36 @@
 var wins = 0
 var loses = 0
 var guessesleft = 10
-var wronletters = []
-var alphabet = ["a", "b", "c","f","d","g","e","h","i"]
+var wrongletters = []
+var alphabet = ["a", "b", "c", "f", "d", "g", "e", "h", "i", "v", "o", "j", "s", "u", "t"]
 var computerguess = alphabet[Math.floor(Math.random() * alphabet.length)]
 console.log(computerguess)
 
 document.onkeyup = function (event) {
-
-    var userchoice = event.key
     
+    var userchoice = event.key
+
     if (userchoice === computerguess) {
         wins++;
         alert("you are right the letter is " + computerguess);
-        
-        reset(); }
+        reset();
+    }
     else {
         guessesleft--;
         document.querySelector("#left").innerHTML = guessesleft;
-        wronletters.push(userchoice);
-        document.querySelector("#wrong").innerHTML = wronletters.join(" ")
+        wrongletters.push(userchoice);
+        document.querySelector("#wrong").innerHTML = wrongletters.join(" ")
         if (guessesleft === 0) {
             loses++
-            alert("you looose"); reset()
+            alert("you looose the letter is " + computerguess);
+            reset()
         };
     };
 };
 function reset() {
+   
     guessesleft = 10;
-    wronletters = [];
+    wrongletters = [];
     computerguess = alphabet[Math.floor(Math.random() * alphabet.length)];
     document.querySelector("#wins").innerHTML = wins;
     document.querySelector("#lose").innerHTML = loses;
